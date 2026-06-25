b323d142c6ec8647ce89f98d1f86dadcf1909c3a
yuhao988 <huan0268@e.ntu.edu.sg>
2026-06-25 11:28:42 +0800

    minor change

--- src/BattleSim.js
+++ src/BattleSim.js
@@ -16,7 +16,27 @@ function BattleSim(prop) {
       contentLabel="Battle Simulation"
     >
       <h2>Battle Simulation</h2>
-      <p>Battle simulation content goes here...</p>
+      
+        <div
+          className="selection-container"
+          style={{ display: "flex", gap: "40px", justifyContent: "center" }}
+        >
+          <div
+            className="selection-box"
+            style={{ border: "2px solid black", padding: "5px " }}
+          >
+            <label htmlFor="yourMonstie">You: </label>
+            
+          </div>
+
+          <div
+            className="selection-box"
+            style={{ border: "2px solid black", padding: "5px " }}
+          >
+            <label htmlFor="oppoMonstie">Opponent: </label>
+            
+          </div>
+        </div>
       <button onClick={handleCloseModal}>Close</button>
     </Modal>
   );
